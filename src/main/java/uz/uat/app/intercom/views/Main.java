83390d576665d307064c019b64dac1b58de74ca4
usmanov1970 <usmanov.ivoice@outlook.com>
2024-02-12 23:41:09 +0500

    c1

--- src/main/java/uz/uat/app/intercom/views/Main.java
+++ src/main/java/uz/uat/app/intercom/views/Main.java
@@ -1,7 +1,28 @@
 package uz.uat.app.intercom.views;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 
 public class Main extends AppLayout {
+    private H2 viewTitle;
+    public Main(){
+        setPrimarySection(Section.DRAWER);
+        addDrawerContent();
+        addHeaderContent();
+    }
 
+    private void addDrawerContent() {
+        // TODO Auto-generated method stub
+      //  throw new UnsupportedOperationException("Unimplemented method 'decorateDrawer'");
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.setAriaLabel("Menu toggle");
+        viewTitle = new H2();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        addToNavbar(true, toggle, viewTitle);
+    }
 }
--- src/main/java/uz/uat/app/intercom/views/message/MessagesView.java
+++ src/main/java/uz/uat/app/intercom/views/message/MessagesView.java
@@ -1,19 +1,28 @@
 package uz.uat.app.intercom.views.message;
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
 import uz.uat.app.intercom.views.Main;
 
-@PageTitle("Hello World")
-@Route(value = "hello message", layout = Main.class)
+@PageTitle("UAT Интерком")
+@Route(value = "messages-board", layout = Main.class)
 @RouteAlias(value = "", layout = Main.class)
-public class MessagesView extends VerticalLayout{
+public class MessagesView extends Div implements AfterNavigationObserver{
 
     public MessagesView(){
         add(new H3("Hello"));
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent arg0) {
+        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'afterNavigation'");
     }
 }
