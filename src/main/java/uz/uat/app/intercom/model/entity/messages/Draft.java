30ee95375a1d0a4656805090ec170e7f817f16fc
usmanov1970 <usmanov.ivoice@outlook.com>
2024-02-18 00:25:33 +0500

    c1

--- src/main/java/uz/uat/app/intercom/model/entity/messages/Draft.java
+++ src/main/java/uz/uat/app/intercom/model/entity/messages/Draft.java
@@ -1,17 +1,7 @@
 package uz.uat.app.intercom.model.entity.messages;
 
-import org.springframework.data.annotation.Id;
-
-import com.arangodb.springframework.annotation.ArangoId;
 import com.arangodb.springframework.annotation.Document;
 
-import lombok.Data;
-
-@Data
 @Document(collection = "drafts")
 public class Draft extends Message {
-    @Id
-    private String id;
-    @ArangoId
-    private String arangoId;
 }
