3a6ee90f9a5fb3178148a4790e64c5cf0eb94db1
usmanov1970 <usmanov.ivoice@outlook.com>
2024-02-18 09:19:15 +0500

    c1

--- src/main/java/uz/uat/app/intercom/model/entity/messages/DraftRepository.java
+++ src/main/java/uz/uat/app/intercom/model/entity/messages/DraftRepository.java
@@ -1,7 +1,13 @@
 package uz.uat.app.intercom.model.entity.messages;
 
+import java.util.List;
+
+import org.springframework.data.repository.query.Param;
+
+import com.arangodb.springframework.annotation.Query;
 import com.arangodb.springframework.repository.ArangoRepository;
 
 public interface DraftRepository extends ArangoRepository<Draft, String> {
-
+    @Query(value = "for i in drafts filter i.channel == @channel return i")
+    public List<Draft> findByChannel(@Param(value = "channel") String channel);
 }
